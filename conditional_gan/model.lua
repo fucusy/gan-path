467f24900326643f5635662ed97c5def3ce175ef
fucus <fucus@qq.com>
2017-11-11 22:34:08 +0800

    train without mini batch

--- conditional_gan/model.lua
+++ conditional_gan/model.lua
@@ -31,15 +31,17 @@ function get_model()
 
     local imgBranch = nn.Sequential()
     imgBranch:add(nn.Reshape(32*32))
-    imgBranch:add(nn.Linear(32*32, 240))
+
+
+    imgBranch:add(nn.Maxout(32*32, 240, 5))
     imgBranch:add(nn.ReLU())
-    imgBranch:add(nn.Linear(240, 240))
+    imgBranch:add(nn.Maxout(240, 240, 4))
     imgBranch:add(nn.ReLU())
 
     local classBranch = nn.Sequential()
-    classBranch:add(nn.Linear(10, 50))
+    classBranch:add(nn.Maxout(10, 50, 5))
     classBranch:add(nn.ReLU())
-    classBranch:add(nn.Linear(50, 240))
+    classBranch:add(nn.Maxout(50, 240, 4))
     classBranch:add(nn.ReLU())
 
     p:add(imgBranch)
